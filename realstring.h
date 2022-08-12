986e636bb9080bd8d86ed349e9624062c684e5b9
EXSERENS <7828319+EXSERENS@users.noreply.github.com>
2022-08-12 16:34:46 +0800

    realstring: refine usage method

--- realstring.h
+++ realstring.h
@@ -1,6 +1,7 @@
+#include <string.h>
+
 struct string;
 
-struct string string_init();
+struct string *string_init();
 void string_delete(struct string *realstr);
-void string_reinit(struct string *realstr);
 void string_append(struct string *dest, const char *str);
